a1fcc2ad6ace1a3efa8d165c52c921777664b719
tomasmonge2704 <tomas.monge.vidal@gmail.com>
2021-09-08 06:19:07 -0300

    fix

--- js/scripts.js
+++ js/scripts.js
@@ -7,62 +7,71 @@ class Producto {
         this.cantidad = cantidad;
     }
 }
+/*Productos harcodeados*/
+const Producto1 = new Producto("Adidas","500", "5");
+const Producto2 = new Producto("Nike","500", "5");
+const Producto3 = new Producto("PUMA","500", "5");
+const Producto4 = new Producto("John Foos","500", "5");
 
-const Producto1 = new Producto("Zapatillas","50", "5");
-const Producto2 = new Producto("Zapatillas","50", "5");
-const Producto3 = new Producto("Zapatillas","50", "5");
-const Producto4 = new Producto("Zapatillas","50", "5");
-
+/*Array productos*/
 const productos = [Producto1,Producto2,Producto3,Producto4];
 
-/*suma del precio total*/
-let precio1 = Producto1.precio * Producto1.cantidad
-let precio2 = Producto2.precio * Producto2.cantidad
-let precio3 = Producto3.precio * Producto3.cantidad
-let precio4 = Producto4.precio * Producto4.cantidad
-let preciototal = precio1 + precio2 + precio3 + precio4;
-alert ("Precio total = " + preciototal);
-console.log(productos);
-/*LOCAL STORAGE*/
+if (localStorage.getItem('listaProductos')) {
+    
+}
+else {
 
-localStorage.setItem("PRO1", JSON.stringify(Producto1));
-localStorage.setItem("PRO2", JSON.stringify(Producto2));
-localStorage.setItem("PRO3", JSON.stringify(Producto3));
-localStorage.setItem("PRO4", JSON.stringify(Producto4));
+/*LOCALSTORAGE*/
+localStorage.setItem("listaProductos", JSON.stringify(productos));
+}
+
 /* DOM */
 
-document.getElementById('pr1').innerHTML = `<h5 class="fw-bolder">  ${Producto1.nombre}</h5>
-                        <p>  $${Producto1.precio}</p>
-                        <p>  cantidad: ${Producto1.cantidad}</p>`;
+/*carga todos los productos que esten en el localstorage*/
 
-document.getElementById('pr2').innerHTML = `<h5 class="fw-bolder">  ${Producto2.nombre}</h5>
-                        <p>  $${Producto2.precio}</p>
-                        <p>  cantidad: ${Producto2.cantidad}</p>`;     
-document.getElementById('pr3').innerHTML = `<h5 class="fw-bolder">  ${Producto3.nombre}</h5>
-                        <p>  $${Producto3.precio}</p>
-                        <p>  cantidad: ${Producto3.cantidad}</p>`;
-document.getElementById('pr4').innerHTML = `<h5 class="fw-bolder">  ${Producto4.nombre}</h5>
-                        <p>  $${Producto4.precio}</p>
-                        <p>  cantidad: ${Producto4.cantidad}</p>`;
-
-
+function cargarelementosdellocal() {
+    
+    const almacenados = JSON.parse(localStorage.getItem("listaProductos"));
+    for (const producto of almacenados) {
+        /*creo Div*/
+        let div = document.createElement('div');
+        const productList = document.getElementById("product-list");
+        const element = document.createElement("div");
+        element.innerHTML = `<div class="col mb-5">
+        <div class="card h-100">
+            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+            <div class="card-body p-4">
+            <div class="col mb-5">
+            <h5 class="fw-bolder">  ${producto.nombre}</h5>
+            <p>  $${producto.precio}</p>
+            <p>  cantidad: ${producto.cantidad}</p>
+            </div>
+            </div>
+            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Agregar al carrito</a></div>
+            </div>
+        </div>
+    </div>
+        `;
+        productList.appendChild(element);
+    }
+    
+}
+cargarelementosdellocal();
 
 function nuevoProducto() {
+    /*Obtengo datos del input*/
     let nomb = document.getElementById("nomb").value;
     let pre = document.getElementById("pre").value;
     let cant = document.getElementById("cant").value;
-    const Producto5 = new Producto(nomb,pre,cant);
-
+    
+    /*creo Div*/
     let div = document.createElement('div');
-
     const productList = document.getElementById("product-list");
     const element = document.createElement("div");
     element.innerHTML = `<div class="col mb-5">
-    
     <div class="card h-100">
-        
         <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-       
         <div class="card-body p-4">
         <div class="col mb-5">
         <h5 class="fw-bolder">  ${nomb}</h5>
@@ -70,15 +79,24 @@ function nuevoProducto() {
         <p>  cantidad: ${cant}</p>
         </div>
         </div>
-      
         <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
             <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Agregar al carrito</a></div>
         </div>
     </div>
 </div>
     `;
+    /*creo el objeto del producto nuevo*/
+    const Producto5 = new Producto(nomb,pre,cant);
     productList.appendChild(element);
+    /*Guardo en el Array*/
+    productos.push(Producto5);
+    console.log(productos);  
+    /*Guardo en el local storage*/
     localStorage.setItem((nomb), JSON.stringify(Producto5))
+    localStorage.setItem("listaProductos", JSON.stringify(productos));
     }  
-    
-   
+
+
+
+
+console.log(productos);  
